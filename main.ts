9e7f31e1946380a7fcbdb310a28fe107da752e6a
joy410975021 <71752080+joy410975021@users.noreply.github.com>
2020-11-04 18:00:28 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,10 +2,10 @@ basic.forever(function () {
     for (let i = 0; i <= 4; i++) {
         for (let j = 0; j <= i; j++) {
             if (j == 0) {
-                led.toggle(4 - i, i)
+                led.plot(4 - i, i)
             } else {
-                led.toggle(4 - (i - j), i)
-                led.toggle(4 - i, i - j)
+                led.plot(4 - (i - j), i)
+                led.plot(4 - i, i - j)
             }
         }
         basic.pause(500)
